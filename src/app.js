dba0c5b2088060967d171562a4f27fb7eb8151d1
Rishabh-2digit <rishabhtiwari73096@gmail.com>
2026-02-02 18:05:05 +0530

    fix issue

--- src/app.js
+++ src/app.js
@@ -5,19 +5,17 @@ const morgan = require("morgan");
 const connectDB = require("@config/db");
 const passport = require("passport");
 
+require("module-alias/register");
 require("dotenv").config();
-
 require("@config/passport")();
 
 const app = express();
-
 connectDB();
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
-
 app.use(passport.initialize());
 
 const routes = require('./routes');
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 const authRoutes = require("@routes/authRoutes");
 const contactRoutes = require("@routes/contactusRoutes");
-const categoryRoutes = require("@routes/CatgeoryRoutes");
+const categoryRoutes = require("@routes/catgeoryRoutes");
 const productRoutes = require("@routes/productRoutes");
-const clothtypeRoutes = require("@routes/ClothTypeRoutes");
-const StripeRoutes = require("@routes/StripeRoutes");
+const clothtypeRoutes = require("@routes/clothTypeRoutes");
+const stripeRoutes = require("@routes/stripeRoutes");
 
 module.exports = (app) => {
   app.use("/auth", authRoutes);
   app.use("/contactus", contactRoutes);
   app.use("/category", categoryRoutes);
-  app.use("/product",productRoutes);
-  app.use("/clothtype",clothtypeRoutes);
-  app.use("/stripe",StripeRoutes);
+  app.use("/product", productRoutes);
+  app.use("/clothtype", clothtypeRoutes);
+  app.use("/stripe", stripeRoutes);
 };
